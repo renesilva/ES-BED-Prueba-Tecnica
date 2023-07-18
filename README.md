d283411bce3993f4699bf179cb782ebf1c62acb4
ERESSEA RS <rsilva@eresseasolutions.com>
2023-07-18 17:39:15 -0400

    Texto README y moviendo carpetas y nombres

---
+++ README.md
@@ -0,0 +1,16 @@
+Base para ejercicios
+====================
+Comandos para correr los ejercicios base.
+
+Instalación
+-----------
+```
+composer install
+```
+
+Tests
+-----
+Para correr los tests de los ejercicios deben correr
+```
+./vendor/bin/phpunit --verbose --colors tests
+```
--- tests/Eressea/Ejercicios/EjercicioPruebaTest.php
+++ tests/Eressea/Ejercicios/EjercicioPruebaTest.php
@@ -3,11 +3,11 @@ namespace Eressea\Ejercicios;
 
 use PHPUnit\Framework\TestCase;
 
-final class EjercicioPrueba extends TestCase
+final class EjercicioPruebaTest extends TestCase
 {
   public function testTwoSum()
   {
-    $ej1 = new Ejercicio1();
+    $ej1 = new EjercicioPrueba();
     $this->assertEquals([0, 1], $ej1->twoSum([2, 7, 11, 15], 9));
     $this->assertEquals([1, 2], $ej1->twoSum([3, 2, 4], 6));
     $this->assertEquals([0, 1], $ej1->twoSum([3, 3], 6));
